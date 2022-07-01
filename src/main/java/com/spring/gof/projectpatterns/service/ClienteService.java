0348ecae4de3082ede260a2ef6239c10944f1504
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-01 11:21:54 -0300

    Added methods to insert, getall, update and delete clients

---
+++ src/main/java/com/spring/gof/projectpatterns/service/ClienteService.java
@@ -0,0 +1,11 @@
+package com.spring.gof.projectpatterns.service;
+
+import com.spring.gof.projectpatterns.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+}
---
+++ src/main/java/com/spring/gof/projectpatterns/service/ViaCepService.java
@@ -0,0 +1,15 @@
+package com.spring.gof.projectpatterns.service;
+
+import com.spring.gof.projectpatterns.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
